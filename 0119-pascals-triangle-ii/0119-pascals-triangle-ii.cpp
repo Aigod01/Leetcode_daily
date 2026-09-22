12f160364edd6f427c83d3e5b54994a54b3358f9
Aigod01 <awadhnarayanvishwakarma2@gmail.com>
2026-09-22 22:42:32 +0530

    Time: 1 ms (11.32%), Space: 9.2 MB (22.01%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> getRow(int row) {
+        vector<vector<int>>ans;
+
+        for(int i = 0;i<=row;i++){
+            vector<int>temp(i+1,1);
+            
+
+            for(int j = 1;j<i;j++){
+                temp[j] = ans[i-1][j-1] + ans[i-1][j];
+            }
+            ans.push_back(temp);
+        }
+        return ans[row];
+        
+        
+    }
+};
